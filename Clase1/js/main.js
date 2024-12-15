2a28a8a04adcfbae8f0bab032234e1e6d58453f9
Victorfernandolopez <victorfernandolopez1988@gmail.com>
2024-12-15 17:39:31 -0300

    primer commit

---
+++ Clase1/js/main.js
@@ -0,0 +1,3 @@
+console.log('bienvenidos al curso de java script avanzado')
+console.warn('bienvenidos al curso de java script avanzado')
+console.error('bienvenidos al curso de java script avanzado')
